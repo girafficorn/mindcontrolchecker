2b99d9a91cc0acc210f865dc7a56d8bf3c8560f5
girafficorn <kevinrogers85212@gmail.com>
2024-06-18 16:32:58 -0700

    Renames statement-text to hidden-text

--- functions.js
+++ functions.js
@@ -1,24 +1,24 @@
 const infoButtons = document.querySelectorAll(".info-button");
-const statementTexts = document.querySelectorAll(".statement-text");
+const hiddenTexts = document.querySelectorAll(".hidden-text");
 
-// Function to hide all statement texts
-function hideAllStatementTexts() {
-  statementTexts.forEach(text => text.style.display = "none");
+// Function to hide all hidden texts
+function hideAllHiddenTexts() {
+  hiddenTexts.forEach(text => text.style.display = "none");
 }
 
-// Toggle statement text on button click
+// Toggle hidden text on button click
 infoButtons.forEach(button => {
   button.addEventListener("click", function() {
-    const statementText = this.parentNode.querySelector(".statement-text");
+    const hiddenText = this.parentNode.querySelector(".hidden-text");
     // Hide all other statement texts before toggling the clicked one
-    hideAllStatementTexts();
-    statementText.style.display = statementText.style.display === "none" ? "block" : "none";
+    hideAllHiddenTexts();
+    hiddenText.style.display = hiddenText.style.display === "none" ? "block" : "none";
   });
 });
 
-// Hide statement texts on document click (excluding info buttons)
+// Hide hidden texts on document click (excluding info buttons)
 document.addEventListener("click", function(event) {
   if (!event.target.classList.contains("info-button")) {
-    hideAllStatementTexts();
+    hideAllHiddenTexts();
   }
 });
